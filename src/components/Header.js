60040e8f8e8b7a6db12297045004d1a01c56c93f
htppsvitor <vitor.qn2004@gmail.com>
2024-10-10 08:57:32 -0300

    feat:Header feature

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
 
+
 function Header() {
     return (
         <div className="header">
-          <h1>Hello World</h1>
+          <h1>Hello Worldd</h1>
+          <h2>The new era</h2>
         </div>
       );
 }
